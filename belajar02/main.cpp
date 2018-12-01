12a6bcd126a8abc66e634a1e19fe121988c68944
arif hanifanudin <arif.hanifanudin@gmail.com>
2018-12-01 18:49:59 +0700

    variable lokal

---
+++ belajar02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void hitung()
+{int total =1;
+    total *=5;
+    cout << "total pada lain()=" << total << endl;
+}
+int main(){
+    int total =2;
+    total++;
+    hitung();
+    cout << "total pada main()=" << total << endl;
+    cout << "succes!" << endl;
+    return 0;
+}
